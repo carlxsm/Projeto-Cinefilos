6ce5352ec2af846a1a3b3ed250bfc3d64e9d95df
smurlocky <mvinicius.avelino@hotmail.com>
2024-04-10 11:08:38 -0300

    cria chamada de funções na main, classe Sala

--- Cinefilos/src/Main.java
+++ Cinefilos/src/Main.java
@@ -1,5 +1,24 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+
+import model.cinema.Filme;
+import model.cinema.Sala;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Filme meuFilme = new Filme("inception");
+        meuFilme.setHoraFilme(LocalTime.of(18, 30));
+        meuFilme.setDataFilme(LocalDate.of(2024, 4, 15));
+
+        Sala salaA = new Sala(20, null, "A", "Inception", null);
+
+        meuFilme.adicionarSalaExibicao(salaA);
+
+        System.out.println(
+                " Filme: " + meuFilme.getNomeFilme() + "\n Horario de exibição: " + meuFilme.getHoraFilme() + " "
+                        + meuFilme.getDataFilme());
+
+        System.out.println();
     }
 }
--- Cinefilos/src/model/cinema/TipoSala.java
+++ Cinefilos/src/model/cinema/TipoSala.java
@@ -1,9 +1,12 @@
 package model.cinema;
 
 public enum TipoSala {
-    BASICA(0), SALA3D(1),VIP(2);
+    BASICA(0), SALA3D(1), VIP(2);
+
+    private int value;
 
     TipoSala(int value) {
+        this.value = value;
     }
 
 }
