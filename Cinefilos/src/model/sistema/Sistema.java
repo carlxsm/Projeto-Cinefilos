acc45ce5c7e59631c7b9d2f8deee28368388b1f2
Carlos Mendes <58920321+CarlosMendes2@users.noreply.github.com>
2024-04-11 19:29:21 -0300

    TO DOENTE

--- Cinefilos/src/model/sistema/Sistema.java
+++ Cinefilos/src/model/sistema/Sistema.java
@@ -5,10 +5,23 @@ import model.sistema.usuario.Usuario;
 import java.util.ArrayList;
 
 public class Sistema {
+    private static int LOGADO = 0;
+    private static boolean LOGADO = false;
+
     private final ArrayList<Usuario> usuariosCadastrados = new ArrayList<>();
 
     public Sistema() {}
 
+    public boolean isLogado(){
+        if (LOGADO){
+            return true;
+        }else {
+            return false;
+        }
+    }
+    public void desloga(){
+        LOGADO = false;
+    }
     public void logar(){
         //TODO
     }
