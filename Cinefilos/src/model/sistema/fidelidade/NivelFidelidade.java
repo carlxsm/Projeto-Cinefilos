5e0b89d56432def49cbd0248a1ff99c6571ed11f
smurlocky <mvinicius.avelino@hotmail.com>
2024-04-24 21:34:40 -0300

    mantem classe nivel de fidelidade

--- Cinefilos/src/model/sistema/fidelidade/NivelFidelidade.java
+++ Cinefilos/src/model/sistema/fidelidade/NivelFidelidade.java
@@ -1,10 +1,11 @@
 package model.sistema.fidelidade;
 
 public enum NivelFidelidade {
-    SILVER(0),GOLD(1),PLATINUM(2),BLACK(3);
+    SILVER(0), GOLD(1), PLATINUM(2), BLACK(3);
+
     private int value;
 
-    //TODO Desconto
+    // TODO Desconto
     NivelFidelidade(int value) {
         this.value = value;
     }
