086e862edc01f6578dc09fb481f1e67309055c8e
smurlocky <mvinicius.avelino@hotmail.com>
2024-04-24 21:03:38 -0300

    configura classe cliente

--- Cinefilos/src/Main.java
+++ Cinefilos/src/Main.java
@@ -1,79 +1,82 @@
 import model.sistema.Sistema;
+import model.sistema.fidelidade.NivelFidelidade;
+import model.sistema.usuario.Cliente;
 
 public class Main {
 
     public static void main(String[] args) {
-        Sistema sistema = new Sistema();
-        // tela1View tela1 = new tela1View();
-        // tela1.exibiTela();
-        do{
-            //checa se clicou em criar
-                // chamdaDeCriar();
-            // checa se clicou em login
-                // chamadaDeLogar();
-            // checa se vai sair da aplicação
-                // chamadaDeSair();
-        }while (sistema.isLogado());
-        System.out.println("Hello world!");
+        // Criando um novo cliente com 100 pontos iniciais
+        Cliente cliente = new Cliente("João", "senha123", 1, 100);
+
+        // Verificando os pontos de fidelidade do cliente
+        int pontosAtuais = cliente.getPontos();
+        System.out.println("Pontos de fidelidade do cliente: " + pontosAtuais);
+
+        // Verificando o nível de fidelidade do cliente
+        NivelFidelidade nivel = cliente.getNivelFidelidade();
+        System.out.println("Nível de fidelidade do cliente: " + nivel);
+
+        // Acumulando mais pontos de fidelidade para o cliente
+        cliente.acumulaPontos(50);
+        System.out.println("Pontos de fidelidade do cliente após acumular mais pontos: " + cliente.getPontos());
     }
 
     // Sistema.chamadaDeCriar(){
-        //gerenciaSistema.checaSeGerente()
-        //gerenciaSistema.checaSeCliente()
-        //gerenciaSistema.checaSeVoltou();
+    // gerenciaSistema.checaSeGerente()
+    // gerenciaSistema.checaSeCliente()
+    // gerenciaSistema.checaSeVoltou();
     // }
 
-    //gerenciaSistema.checaSeCliente(){
-        //if(isCliente){
-            //gerenciaSistema.atualizaDados(){
-                // controller.gerenciaSistema.atualizaNomeCliente
-                // controller.gerenciaSistema.atualizaPontos
-                // controller.gerenciaSistema.atualizaNivelFidelidade
-    //      }
-            // if(controle.clicouIngresso()){
-    //          //view.telaIngressoCliente(){
-    //             while(gerenciaSistema.botaoVoltar()){
-    //              voew.InstanciaTelaIngresso();{
-    //    //                  exibirFilmesEmCartaz();
-    //    //                  usuario.carrinhoDeCompras.recebeListaSusprensa()
-    //    //                  usuario.carrinhoDeCompras.recebeQuantidadeDeIngresso()
-    //    //                  usuario.carrinhoDeCompras.exibeTotal()
-    //    //                  usuario.carrinhoDeCompras.exibeDesconto()
-    //    //                  usuario.carrinhoDeCompras.recebeFilme();
-    //    //                  if(carromhpDeCpmpras.buttonAdicionaCarrinho()){
-    //    //                      usuario.carrinhoDeCompras.adicionaFilmeCarrinho()
-    //    //                      controle.voltaTelaCliente();
-    //    //                  }else(carrinhoDeCompras.buttonVoltar()){
-    //    //                      controle.voltaTelaCliente();
-    //    //                  }
-    //    //              }
-    //              }
-    //          if(controle.clicouLanchonete){
-    //              //fluxoLanchonete()
-    //              //if(clicouBotaVoltar())
-    //          }
-    //          if(!usuario.carrinhoCompra.isEmpty){
-    //              //usuario.carrinhoDeCompras.exibeTotal()
-    //              //if(clicouButtonFinalizar()){
-    //                  usuario.carrinhoDeCompras.finalizaCompra();
-    //
-    //              }if(clicouButtonClear()){
-    //                  usuario.carrinhoDeCompras.limpaCarrinho()
-    //                  controle.voltaTelaCliente();
-    //              }
-    //          }else{
-    //
-    //          }
-    //          }
+    // gerenciaSistema.checaSeCliente(){
+    // if(isCliente){
+    // gerenciaSistema.atualizaDados(){
+    // controller.gerenciaSistema.atualizaNomeCliente
+    // controller.gerenciaSistema.atualizaPontos
+    // controller.gerenciaSistema.atualizaNivelFidelidade
     // }
-            // if(controle.clicouLanchonete())
-
-        //else if(isGerente){
-                //
+    // if(controle.clicouIngresso()){
+    // //view.telaIngressoCliente(){
+    // while(gerenciaSistema.botaoVoltar()){
+    // voew.InstanciaTelaIngresso();{
+    // // exibirFilmesEmCartaz();
+    // // usuario.carrinhoDeCompras.recebeListaSusprensa()
+    // // usuario.carrinhoDeCompras.recebeQuantidadeDeIngresso()
+    // // usuario.carrinhoDeCompras.exibeTotal()
+    // // usuario.carrinhoDeCompras.exibeDesconto()
+    // // usuario.carrinhoDeCompras.recebeFilme();
+    // // if(carromhpDeCpmpras.buttonAdicionaCarrinho()){
+    // // usuario.carrinhoDeCompras.adicionaFilmeCarrinho()
+    // // controle.voltaTelaCliente();
+    // // }else(carrinhoDeCompras.buttonVoltar()){
+    // // controle.voltaTelaCliente();
+    // // }
+    // // }
+    // }
+    // if(controle.clicouLanchonete){
+    // //fluxoLanchonete()
+    // //if(clicouBotaVoltar())
+    // }
+    // if(!usuario.carrinhoCompra.isEmpty){
+    // //usuario.carrinhoDeCompras.exibeTotal()
+    // //if(clicouButtonFinalizar()){
+    // usuario.carrinhoDeCompras.finalizaCompra();
+    //
+    // }if(clicouButtonClear()){
+    // usuario.carrinhoDeCompras.limpaCarrinho()
+    // controle.voltaTelaCliente();
+    // }
     // }else{
-        // throw error("")
+    //
     // }
+    // }
+    // }
+    // if(controle.clicouLanchonete())
 
+    // else if(isGerente){
+    //
+    // }else{
+    // throw error("")
+    // }
 
     // }
 }
--- Cinefilos/src/model/sistema/usuario/Cliente.java
+++ Cinefilos/src/model/sistema/usuario/Cliente.java
@@ -3,9 +3,26 @@ package model.sistema.usuario;
 import model.sistema.fidelidade.NivelFidelidade;
 import model.sistema.fidelidade.ProgramaFidelidade;
 
-public class Cliente extends Usuario{
-    public Cliente(String nome, String senha, int categoriaUsuario) {
+public class Cliente extends Usuario {
+
+    private ProgramaFidelidade ProgramaFidelidade;
+    private NivelFidelidade NivelFidelidade;
+
+    public Cliente(String nome, String senha, int categoriaUsuario, int pontosInciais) {
         super(nome, senha, categoriaUsuario);
+        this.ProgramaFidelidade = new ProgramaFidelidade(pontosInciais);
+    }
+
+    public int getPontos() {
+        return ProgramaFidelidade.getPontos();
+    }
+
+    public NivelFidelidade getNivelFidelidade() {
+        return NivelFidelidade;
+    }
+
+    public void acumulaPontos(int pontos) {
+        ProgramaFidelidade.setPontos(ProgramaFidelidade.getPontos() + pontos);
     }
 
 }
