13a34e1c0fd9eeb1719eb028a910d3114c0085fa
smurlocky <mvinicius.avelino@hotmail.com>
2024-04-24 21:32:27 -0300

    testa a classe cliente na main

--- Cinefilos/src/Main.java
+++ Cinefilos/src/Main.java
@@ -5,22 +5,24 @@ import java.util.ArrayList;
 import model.cinema.Filme;
 import model.cinema.Sala;
 import model.lanchonete.ProdutoLanchonete;
+import model.sistema.fidelidade.NivelFidelidade;
+import model.sistema.usuario.Cliente;
 
 public class Main {
     public static void main(String[] args) {
-        Filme meuFilme = new Filme("inception");
-        meuFilme.setHoraFilme(LocalTime.of(18, 30));
-        meuFilme.setDataFilme(LocalDate.of(2024, 4, 15));
-        ProdutoLanchonete meuProduto = new ProdutoLanchonete("Pipoca", 8, 1);
+        // Criando um novo cliente com 100 pontos iniciais
+        Cliente cliente = new Cliente("João", "senha123", 0, 100);
 
-        Sala salaA = new Sala(20, null, "A", "Inception", null);
+        // Verificando os pontos de fidelidade do cliente
+        int pontosAtuais = cliente.getPontos();
+        System.out.println("Pontos de fidelidade do cliente: " + pontosAtuais);
 
-        meuFilme.adicionarSalaExibicao(salaA);
+        // Verificando o nível de fidelidade do cliente
+        NivelFidelidade nivel = cliente.getNivelFidelidade();
+        System.out.println("Nível de fidelidade do cliente: " + nivel);
 
-        System.out.println(
-                " Filme: " + meuFilme.getNomeFilme() + "\n Horario de exibição: " + meuFilme.getHoraFilme() + " "
-                        + meuFilme.getDataFilme());
-
-        System.out.println(meuProduto.getDescricao());
+        // Acumulando mais pontos de fidelidade para o cliente
+        cliente.acumulaPontos(50);
+        System.out.println("Pontos de fidelidade do cliente após acumular mais pontos: " + cliente.getPontos());
     }
 }
