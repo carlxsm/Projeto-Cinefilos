8e3ce712f5ea9b97dfa0f8a327ed068e386a5f4a
Marcos Vinicius S. Avelino <152659313+smurlocky@users.noreply.github.com>
2024-04-30 01:20:12 -0300

    cria lista para armazenar os produtos criados

--- Cinefilos/src/model/lanchonete/ProdutoLanchonete.java
+++ Cinefilos/src/model/lanchonete/ProdutoLanchonete.java
@@ -1,17 +1,24 @@
 package model.lanchonete;
 
 import model.Produto;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProdutoLanchonete extends Produto {
 
-    public ProdutoLanchonete(String nome, double preco, int quantidade,String codigo) {
+    private static List<ProdutoLanchonete> listaProdutos = new ArrayList<>();
+
+    public ProdutoLanchonete(String nome, double preco, int quantidade, String codigo) {
         super(nome, preco, quantidade, codigo);
+        listaProdutos.add(this);
     }
 
+    public static List<ProdutoLanchonete> getListaProdutos() {
+        return listaProdutos;
+    }
 
     @Override
     public String toString() {
-        return super.getNome()+" | "+super.getPreco()+" | "+super.getCodigo();
+        return super.getNome() + " | " + super.getPreco() + " | " + super.getCodigo();
     }
-
 }
