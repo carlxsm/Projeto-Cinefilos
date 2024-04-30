08908c95a18792ffb602d9614f8ab6abd6c3da8e
Marcos Vinicius S. Avelino <152659313+smurlocky@users.noreply.github.com>
2024-04-30 01:20:27 -0300

    adiciona horario ao filme

--- Cinefilos/src/model/cinema/Filme.java
+++ Cinefilos/src/model/cinema/Filme.java
@@ -8,10 +8,12 @@ public class Filme {
 
     private String nomeFilme;
     private int duracao;
+    private int horario;
 
-    public Filme(String nomeFilme,int duracao) {
+    public Filme(String nomeFilme, int duracao, int horario) {
         this.nomeFilme = nomeFilme;
         this.duracao = duracao;
+        this.horario = horario;
     }
 
     public String getNomeFilme() {
@@ -30,8 +32,16 @@ public class Filme {
         this.duracao = duracao;
     }
 
+    public int getHorario() {
+        return horario;
+    }
+
+    public void setHorario(int horario) {
+        this.horario = horario;
+    }
+
     @Override
     public String toString() {
-        return getNomeFilme() +" | "+ getDuracao()+" minutos";
+        return getNomeFilme() + " | " + getDuracao() + " minutos";
     }
 }
