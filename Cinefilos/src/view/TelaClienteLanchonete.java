d67786912b70a749806a8ea8e58d1ec6b161eefc
Marcos Vinicius S. Avelino <152659313+smurlocky@users.noreply.github.com>
2024-04-30 01:18:36 -0300

    cria tela para exbir os produtos da lanchonete

--- Cinefilos/src/view/TelaClienteLanchonete.java
+++ Cinefilos/src/view/TelaClienteLanchonete.java
@@ -1,4 +1,21 @@
 package view;
 
+import model.lanchonete.ProdutoLanchonete;
+
+import java.util.List;
+
 public class TelaClienteLanchonete {
+
+  public void exibirProdutos() {
+    List<ProdutoLanchonete> produtos = ProdutoLanchonete.getListaProdutos();
+
+    if (produtos.isEmpty()) {
+      System.out.println("Não há produtos disponíveis no momento.");
+    } else {
+      System.out.println("Produtos Disponíveis na Lanchonete:");
+      for (ProdutoLanchonete produto : produtos) {
+        System.out.println(produto);
+      }
+    }
+  }
 }
