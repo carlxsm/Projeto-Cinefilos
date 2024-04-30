4c3bfbfa7635e1d969afeed67d85688adc818f07
Carlos Mendes <58920321+CarlosMendes2@users.noreply.github.com>
2024-04-30 14:16:26 -0300

    Revert "cria lista para armazenar os produtos criados"
    This reverts commit 8e3ce712f5ea9b97dfa0f8a327ed068e386a5f4a.

--- Cinefilos/src/model/lanchonete/ProdutoLanchonete.java
+++ Cinefilos/src/model/lanchonete/ProdutoLanchonete.java
@@ -1,24 +1,17 @@
 package model.lanchonete;
 
 import model.Produto;
-import java.util.ArrayList;
-import java.util.List;
 
 public class ProdutoLanchonete extends Produto {
 
-    private static List<ProdutoLanchonete> listaProdutos = new ArrayList<>();
-
-    public ProdutoLanchonete(String nome, double preco, int quantidade, String codigo) {
+    public ProdutoLanchonete(String nome, double preco, int quantidade,String codigo) {
         super(nome, preco, quantidade, codigo);
-        listaProdutos.add(this);
     }
 
-    public static List<ProdutoLanchonete> getListaProdutos() {
-        return listaProdutos;
-    }
 
     @Override
     public String toString() {
-        return super.getNome() + " | " + super.getPreco() + " | " + super.getCodigo();
+        return super.getNome()+" | "+super.getPreco()+" | "+super.getCodigo();
     }
+
 }
