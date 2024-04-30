5b04ddba9f3a951fa9b5cea03025fa1eaf7b62da
Marcos Vinicius S. Avelino <152659313+smurlocky@users.noreply.github.com>
2024-04-30 01:18:05 -0300

    corrige problema que duplicava elementos na lista

--- Cinefilos/src/controller/GerenciaCinema.java
+++ Cinefilos/src/controller/GerenciaCinema.java
@@ -12,7 +12,6 @@ public class GerenciaCinema {
     private static List<Filme> filmesEmCartaz = new ArrayList<>();
     private static List<List<ProdutoIngressoCinema>> ingressosDoCinema = new ArrayList<>();
 
-
     public static List<Sala> getSalasCinema() {
         return salasCinema;
     }
@@ -21,45 +20,48 @@ public class GerenciaCinema {
         return filmesEmCartaz;
     }
 
-    public void adicionaFilmeEmCartaz(String nomeFilme, int duracao) {
-        Filme novoFilme = new Filme(nomeFilme,duracao);
+    public void adicionaFilmeEmCartaz(String nomeFilme, int duracao, int horario) {
+        Filme novoFilme = new Filme(nomeFilme, duracao, horario);
         filmesEmCartaz.add(novoFilme);
     }
 
     public void removeFilmeEmCartaz(Filme filme) {
         filmesEmCartaz.remove(filme);
     }
-    public void adicionaSalaCinema(Sala sala){
+
+    public void adicionaSalaCinema(Sala sala) {
         salasCinema.add(sala);
     }
 
-    public void removeSalaCinema(Sala sala){
+    public void removeSalaCinema(Sala sala) {
         salasCinema.remove(sala);
     }
 
     // TODO esse método parece encaixar melhor no Facade.
-    public void adicionaFilmeNaSala(Sala sala, Filme filme,int horario){
-        criaIngressos(filme,sala,horario);
+    public void adicionaFilmeNaSala(Sala sala, Filme filme, int horario) {
+        criaIngressos(filme, sala, horario);
         sala.adicionarFilme(filme);
     }
 
-    public void criaIngressos(Filme filme, Sala sala, int horario){
+    public void criaIngressos(Filme filme, Sala sala, int horario) {
         List<ProdutoIngressoCinema> ingressos = new ArrayList<>();
-        String codigoIngresso = "C"+GerenciaSistema.geraCodigo();
-        for (int i = 0; i < sala.getTipoSala().getQuantidadePoltronas();i++){
-            ingressos.add(new ProdutoIngressoCinema(filme,sala,i,horario,codigoIngresso));
+        String codigoIngresso = "C" + GerenciaSistema.geraCodigo();
+        for (int i = 0; i < sala.getTipoSala().getQuantidadePoltronas(); i++) {
+            ingressos.add(new ProdutoIngressoCinema(filme, sala, i, horario, codigoIngresso));
         }
         ingressosDoCinema.add(ingressos);
     }
 
-    public List<String> getIngressosDisponiveis(){
+    public List<String> getIngressosDisponiveis() {
         List<String> ingressosDisponiveis = new ArrayList<>();
         Set<String> chavesUnicas = new HashSet<>();
-        for (List<ProdutoIngressoCinema> ingresso: ingressosDoCinema){
-            for (ProdutoIngressoCinema prodIngresso: ingresso){
-                String chaveUnica = prodIngresso.getFilme().getNomeFilme()+ " | "+prodIngresso.getSala().getNomeSala() +" | "+ prodIngresso.getHorario()+ " | "
-                        +"Cod: "+ prodIngresso.getCodigo()+"| Ingressos disponiveis:  "+ quantidadeDeIngressosDisponiveis(prodIngresso.getCodigo());
-                if (!chavesUnicas.contains(chaveUnica)){
+        for (List<ProdutoIngressoCinema> ingresso : ingressosDoCinema) {
+            for (ProdutoIngressoCinema prodIngresso : ingresso) {
+                String chaveUnica = prodIngresso.getFilme().getNomeFilme() + " | "
+                        + prodIngresso.getSala().getNomeSala() + " | " + prodIngresso.getHorario() + " | "
+                        + "Cod: " + prodIngresso.getCodigo() + "| Ingressos disponiveis:  "
+                        + quantidadeDeIngressosDisponiveis(prodIngresso.getCodigo());
+                if (!chavesUnicas.contains(chaveUnica)) {
                     ingressosDisponiveis.add(chaveUnica);
                     chavesUnicas.add(chaveUnica);
                 }
@@ -68,11 +70,11 @@ public class GerenciaCinema {
         return ingressosDisponiveis;
     }
 
-    public int quantidadeDeIngressosDisponiveis(String codigo){
+    public int quantidadeDeIngressosDisponiveis(String codigo) {
         int quantidade = 0;
-        for (List<ProdutoIngressoCinema> ingresso: ingressosDoCinema){
-            for (ProdutoIngressoCinema prodIngresso: ingresso){
-                if (prodIngresso.getCodigo().equals(codigo)){
+        for (List<ProdutoIngressoCinema> ingresso : ingressosDoCinema) {
+            for (ProdutoIngressoCinema prodIngresso : ingresso) {
+                if (prodIngresso.getCodigo().equals(codigo)) {
                     quantidade++;
                 }
             }
@@ -80,16 +82,17 @@ public class GerenciaCinema {
         return quantidade;
     }
 
-    public static Produto getIngressoPorCodigo(String cod){
-        for (List<ProdutoIngressoCinema> ingresso: ingressosDoCinema){
-            for (ProdutoIngressoCinema prodIngresso: ingresso){
-                if (prodIngresso.getCodigo().equalsIgnoreCase(cod)){
+    public static Produto getIngressoPorCodigo(String cod) {
+        for (List<ProdutoIngressoCinema> ingresso : ingressosDoCinema) {
+            for (ProdutoIngressoCinema prodIngresso : ingresso) {
+                if (prodIngresso.getCodigo().equalsIgnoreCase(cod)) {
                     return prodIngresso;
                 }
             }
         }
         return null;
-        //throw new IllegalArgumentException("Não existe ingresso com código informado.");
+        // throw new IllegalArgumentException("Não existe ingresso com código
+        // informado.");
     }
 
     // OBSERVACAO
@@ -106,18 +109,19 @@ public class GerenciaCinema {
                 }
             }
 
-            // Se nenhum ingresso foi removido na iteração atual, verifica se a lista está vazia
+            // Se nenhum ingresso foi removido na iteração atual, verifica se a lista está
+            // vazia
             if (!ingressoRemovido && listaIngressos.isEmpty()) {
                 iterator.remove(); // Remove a lista vazia se não houver mais ingressos
             }
         }
     }
 
-
-    public Sala getSalaCinema(int indice){
+    public Sala getSalaCinema(int indice) {
         return salasCinema.get(indice);
     }
-    public Filme getFilmeNaSala(int indice){
+
+    public Filme getFilmeNaSala(int indice) {
         return filmesEmCartaz.get(indice);
     }
 
--- Cinefilos/src/model/cinema/Sala.java
+++ Cinefilos/src/model/cinema/Sala.java
@@ -1,20 +1,22 @@
 package model.cinema;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Sala {
 
-    private List<Filme> programacaoFilmes = new ArrayList<>(); // TODO esse atributo so está servindo para verificar se o filme já está cadastrado na sala, mas ele não ta legal não, ta bem zoado.
+    // com essa estrutura, nao pode haver elementos repitidos
+    // Set é uma interface que nao permite
+    private Set<Filme> programacaoFilmes = new HashSet<>();
     private String nomeSala;
     private TipoSala tipoSala;
 
-    public Sala( String nomeSala, String tipoSala) {
+    public Sala(String nomeSala, String tipoSala) {
         this.nomeSala = nomeSala;
         this.tipoSala = TipoSala.valueOf(tipoSala);
     }
 
-    public List<Filme> getProgramacaoFilmes() {
+    public Set<Filme> getProgramacaoFilmes() {
         return programacaoFilmes;
     }
 
@@ -30,18 +32,18 @@ public class Sala {
         return tipoSala;
     }
 
-    public int adicionarFilme( Filme filme){
-        if (!programacaoFilmes.contains(filme)){
-            programacaoFilmes.add(filme);
-            return programacaoFilmes.indexOf(filme);
-        }
-        return -1;
+    public void adicionarFilme(Filme filme) {
+        programacaoFilmes.add(filme);
     }
-    public void removeFilme(Filme filme){
+
+    public void removeFilme(Filme filme) {
         programacaoFilmes.remove(filme);
     }
-    public void atualizaFilme(){
-        // TODO FAZER METODO
 
+    public void atualizaFilme(Filme filmeAntigo, Filme filmeNovo) {
+        if (programacaoFilmes.contains(filmeAntigo)) {
+            programacaoFilmes.remove(filmeAntigo);
+            programacaoFilmes.add(filmeNovo);
+        }
     }
 }
