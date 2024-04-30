6163d759c725ca73f35143fe081870389e2a6551
Marcos Vinicius S. Avelino <152659313+smurlocky@users.noreply.github.com>
2024-04-30 01:19:44 -0300

    corrige erro de mismatch de tipo

--- Cinefilos/src/model/sistema/usuario/Usuario.java
+++ Cinefilos/src/model/sistema/usuario/Usuario.java
@@ -1,16 +1,17 @@
 package model.sistema.usuario;
 
+import model.sistema.fidelidade.ProgramaFidelidade;
+
 public abstract class Usuario {
     private String nome;
     private String senha;
     private CategoriaUsuario categoriaUsuario;
 
-    public Usuario(String nome, String senha, CategoriaUsuario categoriaUsuario) {
+    public Usuario(String nome, String senha, CategoriaUsuario categoriaUsuario2) {
         this.nome = nome;
         this.senha = senha;
-        this.categoriaUsuario = categoriaUsuario;
+        this.categoriaUsuario = categoriaUsuario2;
     }
-
 
     public String getNome() {
         return nome;
