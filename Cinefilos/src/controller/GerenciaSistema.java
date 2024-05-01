3679c8bf4cc8b842157abd33e6f98d7f68b320de
Marcos Vinicius S. Avelino <152659313+smurlocky@users.noreply.github.com>
2024-05-01 12:14:12 -0300

    fix: geração de relatorio de vendas

--- Cinefilos/src/controller/GerenciaSistema.java
+++ Cinefilos/src/controller/GerenciaSistema.java
@@ -1,5 +1,6 @@
 package controller;
 
+import model.Produto;
 import model.sistema.Sistema;
 import model.sistema.usuario.CategoriaUsuario;
 import model.sistema.usuario.Cliente;
@@ -12,7 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class GerenciaSistema implements Serializable{
+public class GerenciaSistema implements Serializable {
 
     final Sistema sistema = new Sistema();
     final static List<CarrinhoCompras> totalCompras = new ArrayList<>();
@@ -25,92 +26,100 @@ public class GerenciaSistema implements Serializable{
     }
 
     public void adicionaCompras(CarrinhoCompras carrinhoCompras) {
-        CarrinhoCompras carrinhoComprasClone = new CarrinhoCompras(carrinhoCompras.getTipoCliente(),carrinhoCompras.getData(),
-                carrinhoCompras.getGerenciaSistema(), carrinhoCompras.getGerenciaCinema(),carrinhoCompras.getCarrinhoDeCompras() );
+        List<Produto> produtos = new ArrayList<>(carrinhoCompras.getCarrinhoDeCompras());
+        CarrinhoCompras carrinhoComprasClone = new CarrinhoCompras(carrinhoCompras.getTipoCliente(),
+                carrinhoCompras.getData(),
+                carrinhoCompras.getGerenciaSistema(), carrinhoCompras.getGerenciaCinema(),
+                produtos);
         totalCompras.add(carrinhoComprasClone);
         totalCompras.indexOf(carrinhoComprasClone);
     }
+
     public List<String> relatorioVendas() {
         List<String> relatorioVendas = new ArrayList<>();
         for (CarrinhoCompras carrinho : totalCompras) {
             System.out.println(carrinho);
-            String infoData = carrinho.getData().format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+            String infoData = "";
+            if (carrinho.getData() != null) {
+                infoData = carrinho.getData().format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+            }
             String infoUsuario = carrinho.getTipoCliente().toString();
             String infoValorCompra = String.valueOf(carrinho.valorComDesconto());
-            String infoCompra = infoData+" | " + infoUsuario + " | R$" + infoValorCompra;
+            String infoCompra = infoData + " | " + infoUsuario + " | R$" + infoValorCompra;
             relatorioVendas.add(infoCompra);
         }
         return relatorioVendas;
     }
 
-    public void registrarNovoCliente(String nome, String senha){
+    public void registrarNovoCliente(String nome, String senha) {
         try {
             validaNomeSenha(nome);
             validaNomeSenha(senha);
-        }catch (IllegalArgumentException iae){
+        } catch (IllegalArgumentException iae) {
             throw iae;
         }
-        if (!Sistema.verificaUsuarioExiste(nome)){
-            sistema.adicionaNovoUsuario(new Cliente(nome,senha));
-        }else{
+        if (!Sistema.verificaUsuarioExiste(nome)) {
+            sistema.adicionaNovoUsuario(new Cliente(nome, senha));
+        } else {
             throw new IllegalArgumentException("Usuário já existe");
         }
     }
 
-    public void registraNovoGerente(String nome, String senha){
+    public void registraNovoGerente(String nome, String senha) {
         try {
             validaNomeSenha(nome);
             validaNomeSenha(senha);
-        }catch (IllegalArgumentException iae){
+        } catch (IllegalArgumentException iae) {
             throw iae;
         }
-        if (!Sistema.verificaUsuarioExiste(nome)){
-            sistema.adicionaNovoUsuario(new Gerente(nome,senha));
-        }else{
+        if (!Sistema.verificaUsuarioExiste(nome)) {
+            sistema.adicionaNovoUsuario(new Gerente(nome, senha));
+        } else {
             throw new IllegalArgumentException("Usuário já existe");
         }
     }
 
-    public void fazerLogin(String nome, String senha){
-        for (Usuario user: Sistema.getUsuariosCadastrados()){
-            if (user.getNome().equals(nome) && user.getSenha().equals(senha)){
+    public void fazerLogin(String nome, String senha) {
+        for (Usuario user : Sistema.getUsuariosCadastrados()) {
+            if (user.getNome().equals(nome) && user.getSenha().equals(senha)) {
                 sistema.setLOGADO(user);
             }
         }
-        if (Sistema.getLOGADO() == null){
+        if (Sistema.getLOGADO() == null) {
             throw new IllegalArgumentException("Senha incorreta");
         }
     }
 
-    public void validaNomeSenha(String nome){
-        if (nome.length() < 4 || nome.isBlank()){
+    public void validaNomeSenha(String nome) {
+        if (nome.length() < 4 || nome.isBlank()) {
             throw new IllegalArgumentException("Entrada Inválida");
         }
     }
 
-
-    public CategoriaUsuario getCategoriaUsuario(){
-        if (Sistema.getLOGADO().getCategoriaUsuario() == CategoriaUsuario.CLIENTE){
+    public CategoriaUsuario getCategoriaUsuario() {
+        if (Sistema.getLOGADO().getCategoriaUsuario() == CategoriaUsuario.CLIENTE) {
             return CategoriaUsuario.CLIENTE;
-        }else if (Sistema.getLOGADO().getCategoriaUsuario()  == CategoriaUsuario.GERENTE){
+        } else if (Sistema.getLOGADO().getCategoriaUsuario() == CategoriaUsuario.GERENTE) {
             return CategoriaUsuario.GERENTE;
-        }else{
+        } else {
             return null;
         }
     }
 
-    public void deslogar(){
+    public void deslogar() {
         sistema.setLOGADO(null);
     }
 
-    public void desligaSistema(){
+    public void desligaSistema() {
         sistema.setStatusSistema(false);
     }
-    public Sistema getSistema(){
+
+    public Sistema getSistema() {
         return sistema;
     }
-    public static String geraCodigo(){
+
+    public static String geraCodigo() {
         Random random = new Random();
-        return String.valueOf(random.nextInt(1000,9999));
+        return String.valueOf(random.nextInt(1000, 9999));
     }
 }
